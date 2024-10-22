1457da5089b1d8895d5fd536c410f3f9fdb5f6a7
Soni Hidayatulloh <sonihidayatulloh797@gmail.com>
2024-10-22 22:38:28 +0700

    update dashboard slicing ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8" />
@@ -8,53 +8,37 @@
 
     <!-- CSS -->
     <link rel="shortcut icon" href="{{ asset('assets/img/Logo/icon.png') }}" type="image/x-icon">
-
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-        rel="stylesheet" />
-
-    <link rel="stylesheet" href="{{ asset('assets/css/tailwind.output.css') }}" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <!-- END CSS -->
 
     <!-- JS -->
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-    <script src="{{ asset('assets/js/init-alpine.js') }}"></script>
-    <script src="{{ asset('assets/js/charts-lines.js') }}" defer></script>
-    <script src="{{ asset('assets/js/charts-pie.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
+    <script src="{{ asset('assets/js/script.js') }}"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     @livewireStyles
-
 </head>
 
 <body>
-    <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
 
-        <!-- Desktop sidebar -->
-        {{-- @include('layouts.sidebar') --}}
-        <!-- Mobile sidebar -->
+    <!-- HEADER -->
+    @include('components.header')
+    <!-- END HEADER -->
 
-        <!-- Backdrop (Mobile Sidebar) -->
-        {{-- @include('layouts.sidebarresponsive') --}}
+    <!-- MAIN -->
+    <main class="main-content">
+        @yield('content')
+    </main>
+    <!-- END MAIN -->
 
-        <div class="flex flex-col flex-1 w-full">
-
-            @include('layouts.header')
-
-            <main class="h-full overflow-y-auto">
-
-                @yield('content')
-
-            </main>
-
-        </div>
-
-    </div>
+    <!-- FOOTER -->
+    @include('components.footer')
+    <!-- END FOOTER -->
 
     @livewireScripts
-
 </body>
 
 </html>
---
+++ resources/views/livewire/dashboard.php
@@ -0,0 +1,303 @@
+<!-- <div class="container px-6 mx-auto grid">
+    <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+        Dashboard
+    </h2>
+
+    <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+
+        {{-- <div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+            <div class="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10">
+                <div class="rounded overflow-hidden shadow-lg">
+
+                    <a href="#"></a>
+                    <div class="relative">
+                        <a href="#">
+                            <img class="w-full"
+                                src="https://images.pexels.com/photos/196667/pexels-photo-196667.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
+                                alt="Sunset in the mountains">
+                            <div
+                                class="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+                            </div>
+                        </a>
+                        <a href="#!">
+                            <div
+                                class="absolute bottom-0 left-0 bg-indigo-600 px-4 py-2 text-white text-sm hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                                Photos
+                            </div>
+                        </a>
+
+                        <a href="!#">
+                            <div
+                                class="text-sm absolute top-0 right-0 bg-indigo-600 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                                <span class="font-bold">27</span>
+                                <small>March</small>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="px-6 py-4">
+
+                        <a href="#"
+                            class="font-semibold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out">Best
+                            View in Newyork City</a>
+                        <p class="text-gray-500 text-sm">
+                            The city that never sleeps
+                        </p>
+                    </div>
+                    <div class="px-6 py-4 flex flex-row items-center">
+                        <span href="#" class="py-1 text-sm font-regular text-gray-900 mr-1 flex flex-row items-center">
+                            <svg height="13px" width="13px" version="1.1" id="Layer_1"
+                                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;"
+                                xml:space="preserve">
+                                <g>
+                                    <g>
+                                        <path d="M256,0C114.837,0,0,114.837,0,256s114.837,256,256,256s256-114.837,256-256S397.163,0,256,0z M277.333,256
+                    c0,11.797-9.536,21.333-21.333,21.333h-85.333c-11.797,0-21.333-9.536-21.333-21.333s9.536-21.333,21.333-21.333h64v-128
+                    c0-11.797,9.536-21.333,21.333-21.333s21.333,9.536,21.333,21.333V256z"></path>
+                                    </g>
+                                </g>
+                            </svg>
+                            <span class="ml-1">6 mins ago</span></span>
+                    </div>
+                </div>
+                <div class="rounded overflow-hidden shadow-lg">
+                    <a href="#"></a>
+                    <div class="relative">
+                        <a href="#">
+                            <img class="w-full"
+                                src="https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
+                                alt="Sunset in the mountains">
+                            <div
+                                class="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+                            </div>
+                        </a>
+                        <a href="#!">
+                            <div
+                                class="absolute bottom-0 left-0 bg-indigo-600 px-4 py-2 text-white text-sm hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                                Photos
+                            </div>
+                        </a>
+
+                        <a href="!#">
+                            <div
+                                class="text-sm absolute top-0 right-0 bg-indigo-600 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                                <span class="font-bold">20</span>
+                                <small>March</small>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="px-6 py-4">
+
+                        <a href="#"
+                            class="font-semibold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out">Best
+                            Pizza in Town</a>
+                        <p class="text-gray-500 text-sm">
+                            The collection of best pizza images in Newyork city
+                        </p>
+                    </div>
+                    <div class="px-6 py-4 flex flex-row items-center">
+                        <span href="#"
+                            class="py-1 text-sm font-regular text-gray-900 mr-1 flex flex-row justify-between items-center">
+                            <svg height="13px" width="13px" version="1.1" id="Layer_1"
+                                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;"
+                                xml:space="preserve">
+                                <g>
+                                    <g>
+                                        <path
+                                            d="M256,0C114.837,0,0,114.837,0,256s114.837,256,256,256s256-114.837,256-256S397.163,0,256,0z M277.333,256 c0,11.797-9.536,21.333-21.333,21.333h-85.333c-11.797,0-21.333-9.536-21.333-21.333s9.536-21.333,21.333-21.333h64v-128 c0-11.797,9.536-21.333,21.333-21.333s21.333,9.536,21.333,21.333V256z">
+                                        </path>
+                                    </g>
+                                </g>
+                            </svg>
+                            <span class="ml-1">3 mins read</span></span>
+                    </div>
+                </div>
+                <div class="rounded overflow-hidden shadow-lg">
+
+                    <a href="#"></a>
+                    <div class="relative">
+                        <a href="#">
+                            <img class="w-full"
+                                src="https://images.pexels.com/photos/257816/pexels-photo-257816.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
+                                alt="Sunset in the mountains">
+                            <div
+                                class="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+                            </div>
+                        </a>
+                        <a href="#!">
+                            <div
+                                class="absolute bottom-0 left-0 bg-indigo-600 px-4 py-2 text-white text-sm hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                                Photos
+                            </div>
+                        </a>
+
+                        <a href="!#">
+                            <div
+                                class="text-sm absolute top-0 right-0 bg-indigo-600 px-4 text-white rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                                <span class="font-bold">15</span>
+                                <small>April</small>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="px-6 py-4">
+
+                        <a href="#"
+                            class="font-semibold text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out">Best
+                            Salad Images ever</a>
+                        <p class="text-gray-500 text-sm">
+                            The collection of best salads of town in pictures
+                        </p>
+                    </div>
+                    <div class="px-6 py-4 flex flex-row items-center">
+                        <span href="#"
+                            class="py-1 text-sm font-regular text-gray-900 mr-1 flex flex-row justify-between items-center">
+                            <svg height="13px" width="13px" version="1.1" id="Layer_1"
+                                xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;"
+                                xml:space="preserve">
+                                <g>
+                                    <g>
+                                        <path
+                                            d="M256,0C114.837,0,0,114.837,0,256s114.837,256,256,256s256-114.837,256-256S397.163,0,256,0z M277.333,256 c0,11.797-9.536,21.333-21.333,21.333h-85.333c-11.797,0-21.333-9.536-21.333-21.333s9.536-21.333,21.333-21.333h64v-128 c0-11.797,9.536-21.333,21.333-21.333s21.333,9.536,21.333,21.333V256z">
+                                        </path>
+                                    </g>
+                                </g>
+                            </svg>
+                            <span class="ml-1">6 mins read</span></span>
+                    </div>
+                </div>
+            </div>
+        </div> --}}
+
+
+
+        <!-- Card -->
+        <a href="https://smartx-form.ramand.my.id/" target="blank">
+            <div class="flex mt-10 items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 h-80">
+                <div>
+                    <img class="py-3 rounded-lg object-cover h-40 w-full" src="{{ asset('assets/img/Dashboard/Smart City.JPG') }}" alt="">
+                    <h2 class="mb-2 mt-3 text-lg font-bold text-gray-600 dark:text-gray-400">
+                        SMART CITY
+                    </h2>
+                    <p class="mt-2 text-xs font-medium text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
+                        www.smartcity.com
+                    </p>
+                </div>
+            </div>
+        </a>
+
+        <!-- Card -->
+        <a href="https://smart-building.ptmsp.id/login target="blank"">
+            <div
+                class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 h-80">
+                <div>
+                    <img class="w-full py-3" src="{{ asset('assets/img/Dashboard/Smart Building.JPG') }}" alt="">
+                    <h2 class="mb-2 mt-3 text-lg[15px] font-bold text-gray-600 dark:text-gray-400">
+                        SMART BUILDING
+                    </h2>
+                    <p
+                        class="mt-2 text-xs font-medium text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
+                        www.smartbuilding.com
+                    </p>
+                </div>
+            </div>
+        </a>
+
+        <!-- Card -->
+        <a href="http://167.205.67.248:8501/" target="blank">
+            <div
+                class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 h-80">
+                <div>
+                    <img class="w-full py-3" src="{{ asset('assets/img/Dashboard/Smart Mobiilty.JPG') }}" alt="">
+                    <h2 class="mb-2 mt-3 text-lg[15px] font-bold text-gray-600 dark:text-gray-400">
+                        SMART MOBILTY
+                    </h2>
+                    <p
+                        class="mt-2 text-xs font-medium text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
+                        www.smartmobility.com
+                    </p>
+                </div>
+            </div>
+        </a>
+
+        <!-- Card -->
+        <a href="">
+            <div
+                class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 h-80">
+                <div>
+                    <img class="w-full py-3" src="{{ asset('assets/img/Dashboard/Smart Health Care.JPG') }}" alt="">
+                    <h2 class="mb-2 mt-3 text-lg[15px] font-bold text-gray-600 dark:text-gray-400">
+                        SMART HEALTHCARE
+                    </h2>
+                    <p
+                        class="mt-2 text-xs font-medium text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
+                        www.smarthealthcare.com
+                    </p>
+                </div>
+            </div>
+        </a>
+
+        <!-- Card -->
+        <a href="">
+            <div
+                class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 h-80">
+                <div>
+                    <img class="w-full py-3 rounded-lg" src="{{ asset('assets/img/Dashboard/Smart Payment.jpg') }}" alt="">
+                    <h2 class="mb-2 mt-3 text-lg[15px] font-bold text-gray-600 dark:text-gray-400">
+                        SMART PAYMENT
+                    </h2>
+                    <p
+                        class="mt-2 text-xs font-medium text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
+                        www.smartpayment.com
+                    </p>
+                </div>
+            </div>
+        </a>
+    </div>
+
+    <!-- Charts -->
+    {{-- {{-- <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+        Charts
+    </h2>
+    <div class="grid gap-6 mb-8 md:grid-cols-2">
+        <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+            <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
+                Revenue
+            </h4>
+            <canvas id="pie"></canvas>
+            <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
+                <!-- Chart legend -->
+                <div class="flex items-center">
+                    <span class="inline-block w-3 h-3 mr-1 bg-blue-500 rounded-full"></span>
+                    <span>Shirts</span>
+                </div>
+                <div class="flex items-center">
+                    <span class="inline-block w-3 h-3 mr-1 bg-teal-600 rounded-full"></span>
+                    <span>Shoes</span>
+                </div>
+                <div class="flex items-center">
+                    <span class="inline-block w-3 h-3 mr-1 bg-purple-600 rounded-full"></span>
+                    <span>Bags</span>
+                </div>
+            </div>
+        </div>
+        <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+            <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
+                Traffic
+            </h4>
+            <canvas id="line"></canvas>
+            <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
+                <!-- Chart legend -->
+                <div class="flex items-center">
+                    <span class="inline-block w-3 h-3 mr-1 bg-teal-600 rounded-full"></span>
+                    <span>Organic</span>
+                </div>
+                <div class="flex items-center">
+                    <span class="inline-block w-3 h-3 mr-1 bg-purple-600 rounded-full"></span>
+                    <span>Paid</span>
+                </div>
+            </div>
+        </div>
+    </div> --> --}}
+<!--</div>
