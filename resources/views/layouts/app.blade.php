658b93f12b4bdb5ffe9c59fd922f86d8e1a6675d
Soni Hidayatulloh <sonihidayatulloh797@gmail.com>
2024-10-25 10:25:14 +0700

    delete all mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSS -->
     <link rel="shortcut icon" href="{{ asset('assets/img/Logo/icon.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ mix('css/app.css') }}" rel="stylesheet"> --}}
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <!-- END CSS -->
 
